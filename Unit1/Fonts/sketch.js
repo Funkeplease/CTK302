e8f71434f9237013e365c5185796656797285d6d
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-09-08 12:15:24 -0500

    updated font code

--- Unit1/Fonts/sketch.js
+++ Unit1/Fonts/sketch.js
@@ -3,16 +3,20 @@ let f1, f2, f3 ;
 
 function setup() {
   createCanvas(500, 500);
-  f1 = loadFont("assest/cutenotes.zip");
-  f2 = loadFont("assest/hangingletters.zip");
-  f3 = loadFont("assest/loveplants.zip");
+  f1 = loadFont("assets/cutenotes.ttf");
+  f2 = loadFont("assets/hangedletters.ttf");
+  f3 = loadFont("assets/loveplants.otf");
   textAlign(CENTER)
 }
 
 function draw() {
+  background("pink");
+  fill("black");
   textFont(f1, 48) ;
   text("hello there!", width / 2, 100);
-
-  
+  textFont(f2, 24) ;
+  text("I am Funke Ademola-Aliu", width / 2, 200);
+  textFont(f3, 12) ;
+  text("I love kids", width / 2, 250);
 
 }
