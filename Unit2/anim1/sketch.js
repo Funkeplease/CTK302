4e78717e33c1533213d5422179efa2f2be6381ec
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-09-20 13:47:29 -0500

    updated font anim1

--- Unit2/anim1/sketch.js
+++ Unit2/anim1/sketch.js
@@ -1,14 +1,16 @@
 
 let x = 0;
+let f1;
 
 function setup() {
   createCanvas(500, 500);
+  f1 = loadFont("assets/hangedletters.ttf")
 }
 
 function draw() {
   background("blue");
   fill("pink")
-  textSize(120);
+  textFont(f1, 120);
  text("Hi Fam!", x, 200);
  x = x + 5;
  if (x > width){
