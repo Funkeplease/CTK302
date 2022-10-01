8374d175fb0913d46062850d4f32e0452326d762
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-10-01 18:55:41 -0500

    touches and audioin updated

---
+++ Unit2/touches/sketch.js
@@ -0,0 +1,44 @@
+let numberOfTouches ;
+let i1, i2, i3;
+
+function setup() {
+  createCanvas(400, 400);
+    i1 = loadImage ("assets/blackgirl.jpeg");
+    i2 = loadImage ("assets/happy.jpg");
+    i3 = loadImage ("assets/plant.jpeg");
+    imageMode(CENTER);
+  }
+
+
+function draw() {
+  clear();
+  numberOfTouches = touches.length;
+  text(numberOfTouches + ' touches', 5, 10);
+  
+  switch(numberOfTouches) {
+    case 0: 
+      text("no one is touching the screen", 5, 22) ; 
+      break ;
+      
+      case 1: 
+       text("it's kind of lonely here", 5, 22) ; 
+      // put a picture here
+      image(i1, width / 2, height / 2 - 120, 100, 100);
+      break ;
+      
+      case 2:
+      text("two fingers are touching the screen", 5, 22) ; 
+            // put a picture here
+            image(i2, width / 2, height / 2, 100, 100);
+      break ;
+      
+      case 3:
+     text("three are touching the screen", 5, 22) ; 
+            // put a picture here
+            image(i3, width / 2, height / 2 + 120, 100, 100);
+      break ;
+    
+      
+  }
+  
+}
