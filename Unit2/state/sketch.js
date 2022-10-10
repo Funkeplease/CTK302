2e02258c372a2c0f97ed96d5fa2268a22e27b805
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-10-10 12:32:22 -0500

    updated timer

--- Unit2/state/sketch.js
+++ Unit2/state/sketch.js
@@ -6,42 +6,64 @@ function setup() {
 
 function draw() {
   background(220);
-      
+
   switch (state) {
     case 0:
-       background("pink");
-     // text("case 0", 100, 100);
-       for (let j = 0; j <= height; j += 28) {
-    for (let i = 0; i <= width; i += 25) {
-      rect(i, j, 20, 20);}
-       }
+      background("pink");
+      // text("case 0", 100, 100);
+      for (let j = 0; j <= height; j += 28) {
+        for (let i = 0; i <= width; i += 25) {
+          rect(i, j, 20, 20);
+        }
+      }
       break;
-    
-      case 1:
-       background("blue");
+
+    case 1:
+      background("blue");
       text("case 1", 100, 100);
+      for (let j = 0; j <= height; j += 28) {
+        for (let i = 0; i <= width; i += 25) {
+          rect(i, j, 20, 20);
+          rotate(45)
+        }
+      }
       break;
 
     case 2:
-       background("yellow");
+      fill("yellow");
       text("case 2", 100, 100);
+      for (let j = 0; j <= height; j += 28) {
+        for (let i = 0; i <= width; i += 25) {
+          ellipse(i, j, 40, 40);
+        }
+      }
       break;
-      
-      case 3:
-       background("red");
+
+    case 3:
+      background("red");
       text("case 3", 100, 100);
+      for (let j = 0; j <= height; j += 28) {
+        for (let i = 0; i <= width; i += 25) {
+          ellipse(i, j, 20, 20);
+        }
+      }
       break;
-      
-      case 4:
-       background("brown");
+
+    case 4:
+      background("brown");
       text("case 4", 100, 100);
+      for (let j = 0; j <= height; j += 28) {
+        for (let i = 0; i <= width; i += 25) {
+          triangle(i, j, 20, 20, 40, 40);
+        }
+      }
       break;
   }
 }
 
-   function mouseReleased(){
-     state++ ;
-     if (state > 4){
-       state = 0;
-     }
-   }
+function mouseReleased() {
+  state++;
+  if (state > 4) {
+    state = 0;
+  }
+}
--- Unit2/timer/sketch.js
+++ Unit2/timer/sketch.js
@@ -1,8 +1,12 @@
+let i1, i2, i3;
 let state = 0;
 let timer = 0;
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage("assets/blackgirl.jpeg")
+  i2 = loadImage("assets/happy.jpg")
+  i3 = loadImage("assets/plant.jpeg")
 }
 
 function draw() {
@@ -13,7 +17,8 @@ function draw() {
       background("yellow");
    
       //put image and text
-      
+      image(i1, width/2 - 120, 100, 100);
+      text("I am a girl," 120, 120);
       timer++ ;
       if (timer > 3*60){
         timer = 0;
@@ -24,7 +29,8 @@ function draw() {
        case 1:
       background("brown");
         //put image and text
-      
+        image(i2, width/2, 100, 100);
+        text("I am happy," 120, 120);
       timer++ ;
       if (timer > 7*60){
         timer = 0;
@@ -35,7 +41,8 @@ function draw() {
        case 2:
       background("pink");
         //put image and text
-      
+        image(i3, width/2 + 120, 100, 100);
+        text("and I love plants" 120, 120);
       timer++ ;
       if (timer > 5*60){
         timer = 0;
