524094fb06abc0b430e0c6761049553e6d1dcd71
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-10-12 14:06:39 -0500

    update mp2

--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,7 +1,104 @@
+let f1, f2, f3, f4, f5 ;
+let i1;
+let state = 0;
+let timer = 0;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(400, 400);
+  i1 = loadImage("assets/alarm.png")
+  imageMode(CENTER);
+  f1 = loadFont("assets/handwritten.otf");
+  f2 = loadFont("assets/band.ttf");
+  f3 = loadFont("assets/easter.ttf");
+  f4 = loadFont("assets/hangedletters.ttf");
+  f5 = loadFont("assets/cutenotes.ttf");
+  textAlign(CENTER)
 }
 
 function draw() {
+  background(220);
+  
+  switch(state){
+    case 0:
+      background("#1F85DE");
+        //put font and text
+        
+        fill("black");
+        textFont(f1, 25) ;
+        text("Most Domestic violence cases are not reported and this leads to a spiral of ills. This state machine which is an alarm that goes off after a minute of consistent yealling is designed to enable reporting. Keep yelling for a minute and let's see what happens", 120, 120);
+      timer++ ;
+      if (timer > 5*60){
+        timer = 0;
+        state = 0;
+      }
+      break;
+    case 1:
+      background("#1F25DE");
+   
+      //put font and text
+      
+      fill("black");
+      textFont(f1, 48) ;
+      text("I am a girl", 120, 120);
+      timer++ ;
+      if (timer > 3*60){
+        timer = 0;
+        state = 1;
+      }
+      break;
+      
+       case 2:
+      background("#781FDE");
+        //put font and text
+        
+        fill("black");
+        textFont(f1, 48) ;
+        text("I am happy", 120, 120);
+      timer++ ;
+      if (timer > 7*60){
+        timer = 0;
+        state = 2;
+      }
+      break;
+      
+       case 3:
+      background("#D81FDE");
+        //put font and text
+        
+        fill("black");
+        textFont(f1, 48) ;
+        text("and I love plants", 120, 120);
+      timer++ ;
+      if (timer > 11*60){
+        timer = 0;
+        state = 0;
+      }
+      break;
 
+      case 4:
+      background("#1FDED8");
+        //put image, font and text
+        image(i1, width/2,height/2, 100, 100);
+        fill("black");
+        textFont(f1, 48) ;
+        text("and I love plants", 120, 120);
+      timer++ ;
+      if (timer > 15*60){
+        timer = 0;
+        state = 0;
+
+        case 5:
+          background("#25DE1F");
+            //put , font and text
+  
+            fill("black");
+            textFont(f1, 48) ;
+            text("and I love plants", 120, 120);
+          timer++ ;
+          if (timer > 18*60){
+            timer = 0;
+            state = 0;
+      }
+      break;
+  }
 }
