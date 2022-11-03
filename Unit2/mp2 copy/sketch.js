14803c0415a2f1e918c1afa6c2285fba15c7ab6c
funke ademola-aliu <112496215+Funkeplease@users.noreply.github.com>
2022-11-02 22:49:07 -0500

    updated mp2

--- Unit2/mp2 copy/sketch.js
+++ Unit2/mp2 copy/sketch.js
@@ -1,132 +1,138 @@
-var mic;
-var vol = 0;
-let total = 0;
+let f1, f2, f3, f4, f5 ;
+let mic;
+let vol = 0;
+let i1;
 let state = 0;
+let timer = 0;
 let s1;
-let f1,f2,f3,f4,f5;
+let loudest = 1;
+let yell;
+let aprVol = 0
+let ease = 0.01
 
 function preload(){
   s1 = loadSound("assets/warning.wav");
 }
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(windowWidth, windowHeight);
+  i1 = loadImage("assets/alarm.png")
+  imageMode(CENTER);
   f1 = loadFont("assets/regular.ttf");
   f2 = loadFont("assets/band.ttf");
   f3 = loadFont("assets/easter.ttf");
   f4 = loadFont("assets/hangedletters.ttf");
   f5 = loadFont("assets/cutenotes.ttf");
-  // code for initializing mic in.
-  mic = new p5.AudioIn(); // what does "new" mean?
-  mic.start();
-  angleMode(DEGREES);
-  noStroke();
   textAlign(CENTER);
+  rectMode(CENTER);
+  mic = new p5.AudioIn(); 
+  mic.start();
 }
 
 function draw() {
-  background("purple");
-
-  switch (state) {
+  background(220);
+  vol = mic.getLevel().toFixed(1);
+ // aprVol += (vol - aprVol) * ease;
+  aprVol = vol ;
+  yell = map(aprVol, 0, 1, 0 , 10)
+  switch(state){
     case 0:
-      // text on the screen for debugging
-      fill("white");
-      textSize(15);
-      text("Click to start \n  Most Domestic violence cases are not reported \n and this leads to a spiral of ills. \n This state machine which is an alarm, \n  goes off after a minute of consistent yelling. \n It is designed to enable reporting. \n  Keep yelling for a minute and let's see what happens.",width / 2, height / 2);
+      background("#1F85DE");
+        //put font and text
+        
+        fill("black");
+        textFont(f1, 20) ;
+        text("Most Domestic violence cases are not reported and this leads to a spiral of ills. This state machine which is an alarm that goes off after a minute of consistent yelling is designed to enable reporting. Keep yelling for a minute and let's see what happens", width/2, height/2,400, 200);
+      timer++ ;
+      if (timer > 8*60){
+        timer = 0;
+        state = 1;
+      }
       break;
-
-    case 1:
-      // get the sound input
-      vol = mic.getLevel(); // returned level is between 0 and 1
-
-      total += 3 * vol;
-      //    fill("black");
-      // textSize(10);
-      // text(vol.toFixed(3), 30, 30);
-
-      let secs = int(total / 3);
-
-      // this will determine which quadrant of the circle we are in
-      let whichQuadrant = int(secs / 15);
-      // text(whichQuadrant, 200, 240);
-
-      fill("white");
-      textFont(f1, 20);
-      
-
-      switch (whichQuadrant) {
-          
-        case 0: // 0-15 seconds
-          background("#purple");  // you can change this!
-          if (int(total) > 0) {
-            fill("#5D3FD3");  // you can change this!
-            arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
-          }
-          fill("purple");  // you can change this!
-          textFont(f2, 20);
-          text("Yell!", width / 2, height / 2);
-          break;
-
-        case 1: // 16-30 seconds
-          background("#E6E6FA");
-          fill("#5D3FD3");
-          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
-          fill("white");
-          textFont(f1, 20);
-          text("Hey are you good?", width / 2, height / 2);
-
-          break;
-
-        case 2: // 31-45 seconds
-          background("	#CBC3E3");
-          fill("#5D3FD3");
-          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
-          fill("white");
-          textFont(f5, 20);
-          text("Help is coming", width / 2, height / 2);
-
-          break;
-
-        case 3: // 46-59 seconds
-          background ("#CF9FFF");
-          fill("#5D3FD3");
-          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
-          fill("white");
-          textFont(f5, 20);
-          text("Take deep breaths", width / 2, height / 2);
-          //state++;
-          if (!s1.isPlaying()){
-            s1.play();
-          }
-          if (secs > 59){
-            state = 2;
-          }
-          break;
+      case 1:
+        background("red")
+        textFont(f2, 50) ;
+      text("Yell", width/2, height/2);
+      if (yell > 4){
+        state = 2
       }
 
-      fill("white");
-      textSize(20);
-      text(secs + " seconds", width / 2, height / 2 + 30);
-      if (secs > 59) state = 2;
       break;
 
-    case 2: // some sort of ending
-      background("#5D3FD3	");
-      textFont(f1, 20);
-      text("You did great, you’ll be out of there soon!", width / 2, height / 2);
+    case 2:
+      background("#1F25DE");
+   
+      //put font and text
+      
+      fill("black");
+      textFont(f2, 50) ;
+      text("Hey,are you good?", width/2, height/2);
+      timer++ ;
+      if (timer > 3*60){
+        timer = 0;
+        state = 3;
+      }
+      break;
+      
+       case 3:
+      background("#781FDE");
+        //put font and text
+        
+        fill("black");
+        textFont(f3, 50) ;
+        text("Stay Positve", width/2, height/2);
+      timer++ ;
+      if (timer > 4*60){
+        timer = 0;
+        state = 4;
+      }
+      break;
+      
+       case 4:
+      background("#D81FDE");
+        //put font and text
+        
+        fill("black");
+        textFont(f4, 50) ;
+        text("Take deep breaths", width/2, height/2);
+      timer++ ;
+      if (timer > 6*60){
+        timer = 0;
+        state = 5;
+      }
+      break;
+
+      case 5:
+      background("#1FDED8");
+        //put image, font and text
+        image(i1, width/2,height/2 - 150, 100, 100);
+        fill("black");
+        textFont(f5, 50) ;
+        text("Help is coming!", width/2, height/2);
+      timer++ ;
+      if(!s1.isPlaying()){
+        s1.play() ;
+      }
+      if (timer > 8*60){
+        timer = 0;
+        state = 6;
+      }
+      break;
+
+        case 6:
+          background("#25DE1F");
+            //put , font and text
+  
+            fill("black");
+            textFont(f1, 40) ;
+            text("You are doing great, you'll be out of there soon.",width/2, height/2);
+          timer++ ;
+          if (timer > 10*60){
+            timer = 0;
+            state = 0;
+      }
+      break;
   }
 }
-
-function mouseReleased() {
-  if (state == 0) state = 1;
-  if (state == 2) {
-    total = 0;
-    state = 0;
-  }
-}
-// you need this code for audio programs and also, the user
-// needs to click the screen before the mic input will work.
-
 function touchStarted() {
   getAudioContext().resume();
 }
--- Unit2/mp2/sketch.js
+++ Unit2/mp2/sketch.js
@@ -1,14 +1,10 @@
-let f1, f2, f3, f4, f5 ;
-let mic;
-let vol = 0;
-let i1;
+var mic;
+var vol = 0;
+let total = 0;
 let state = 0;
-let timer = 0;
 let s1;
-let loudest = 1;
-let yell;
-let aprVol = 0
-let ease = 0.01
+let f1,f2,f3,f4,f5;
+let i1;
 
 function preload(){
   s1 = loadSound("assets/warning.wav");
@@ -22,117 +18,121 @@ function setup() {
   f3 = loadFont("assets/easter.ttf");
   f4 = loadFont("assets/hangedletters.ttf");
   f5 = loadFont("assets/cutenotes.ttf");
+  // code for initializing mic in.
+  mic = new p5.AudioIn(); // what does "new" mean?
+  mic.start();
+  angleMode(DEGREES);
+  noStroke();
   textAlign(CENTER);
   rectMode(CENTER);
-  mic = new p5.AudioIn(); 
-  mic.start();
 }
 
 function draw() {
-  background(220);
-  vol = mic.getLevel().toFixed(1);
- // aprVol += (vol - aprVol) * ease;
-  aprVol = vol ;
-  yell = map(aprVol, 0, 1, 0 , 10)
-  switch(state){
+  background("#1F85DE");
+
+  switch (state) {
     case 0:
-      background("#1F85DE");
-        //put font and text
-        
-        fill("black");
-        textFont(f1, 20) ;
-        text("Most Domestic violence cases are not reported and this leads to a spiral of ills. This state machine which is an alarm that goes off after a minute of consistent yelling is designed to enable reporting. Keep yelling for a minute and let's see what happens", width/2, height/2,400, 200);
-      timer++ ;
-      if (timer > 8*60){
-        timer = 0;
-        state = 1;
-      }
-      break;
-      case 1:
-        background("red")
-        textFont(f2, 50) ;
-      text("Yell", width/2, height/2);
-      if (yell > 4){
-        state = 2
-      }
-
-      break;
-
-    case 2:
-      background("#1F25DE");
-   
-      //put font and text
-      
+      // text on the screen for debugging
       fill("black");
-      textFont(f2, 50) ;
-      text("Hey,are you good?", width/2, height/2);
-      timer++ ;
-      if (timer > 3*60){
-        timer = 0;
-        state = 3;
-      }
-      break;
-      
-       case 3:
-      background("#781FDE");
-        //put font and text
-        
-        fill("black");
-        textFont(f3, 50) ;
-        text("Stay Positve", width/2, height/2);
-      timer++ ;
-      if (timer > 4*60){
-        timer = 0;
-        state = 4;
-      }
-      break;
-      
-       case 4:
-      background("#D81FDE");
-        //put font and text
-        
-        fill("black");
-        textFont(f4, 50) ;
-        text("Take deep breaths", width/2, height/2);
-      timer++ ;
-      if (timer > 6*60){
-        timer = 0;
-        state = 5;
-      }
+      textSize(20);
+      text("Click to start",width / 2, height / 2+200);
+      textFont(f1, 25) ;
+     text("Most Domestic violence cases are not reported and this leads to a spiral of ills. This state machine which is an alarm that goes off after a minute of consistent yelling is designed to enable reporting. Keep yelling for a minute and let's see what happens.", width/2, height/2,400, 200);
       break;
 
-      case 5:
-      background("#1FDED8");
-        //put image, font and text
-        image(i1, width/2,height/2 - 150, 100, 100);
-        fill("black");
-        textFont(f5, 50) ;
-        text("Help is coming!", width/2, height/2);
-      timer++ ;
-      if(!s1.isPlaying()){
-        s1.play() ;
+    case 1:
+      // get the sound input
+      vol = mic.getLevel(); // returned level is between 0 and 1
+
+      total += 3 * vol;
+      //    fill("black");
+      // textSize(10);
+      // text(vol.toFixed(3), 30, 30);
+
+      let secs = int(total / 3);
+
+      // this will determine which quadrant of the circle we are in
+      let whichQuadrant = int(secs / 15);
+      // text(whichQuadrant, 200, 240);
+
+      fill("white");
+      textFont(f1, 20);
+      
+
+      switch (whichQuadrant) {
+          
+        case 0: // 0-15 seconds
+          background("red");  // you can change this!
+          if (int(total) > 0) {
+            fill("white");  // you can change this!
+            arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
+          }
+          fill("black");  // you can change this!
+          textFont(f2, 20);
+          text("Yell!", width / 2, height / 2);
+          break;
+
+        case 1: // 16-30 seconds
+          background("#1F25DE");
+          fill("#white");
+          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
+          fill("black");
+          textFont(f1, 20);
+          text("Hey, are you good?", width / 2, height / 2);
+
+          break;
+
+        case 2: // 31-45 seconds
+          background("#D81FDE");
+          fill("white");
+          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
+          fill("black");
+          textFont(f4, 20);
+          text("Take deep breaths...", width / 2, height / 2);
+
+          break;
+
+        case 3: // 46-59 seconds
+          background ("#1FDED8");
+          image(i1, width/2,height/2 + 150, 100, 100);
+          fill("white");
+          arc(width / 2, height / 2, 300, 300, -90, int(total) * 2 - 90, PIE);
+          fill("black");
+          textFont(f5, 20);
+          text("Help is coming!", width / 2, height / 2);
+          //state++;
+          if (!s1.isPlaying()){
+            s1.play();
+          }
+          if (secs > 59){
+            state = 2;
+          }
+          break;
       }
-      if (timer > 8*60){
-        timer = 0;
-        state = 6;
-      }
+
+      fill("black");
+      textSize(20);
+      text(secs + " seconds", width / 2, height / 2 + 30);
+      if (secs > 59) state = 2;
       break;
 
-        case 6:
-          background("#25DE1F");
-            //put , font and text
-  
-            fill("black");
-            textFont(f1, 40) ;
-            text("You are doing great, you'll be out of there soon.",width/2, height/2);
-          timer++ ;
-          if (timer > 10*60){
-            timer = 0;
-            state = 0;
-      }
-      break;
+    case 2: // some sort of ending
+      background("#1F85DE");
+      textFont(f1, 20);
+      text("You did great, you’ll be out of there soon!", width / 2, height / 2);
   }
 }
+
+function mouseReleased() {
+  if (state == 0) state = 1;
+  if (state == 2) {
+    total = 0;
+    state = 0;
+  }
+}
+// you need this code for audio programs and also, the user
+// needs to click the screen before the mic input will work.
+
 function touchStarted() {
   getAudioContext().resume();
 }
